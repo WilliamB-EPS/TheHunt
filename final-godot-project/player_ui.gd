59aa3cdf9be289b135dd1c256ee62ea433df7557
WilliamB-EPS <wbowers@eastsideprep.org>
2023-05-23 12:33:11 -0700

    Upgrading player UI with nicer assets

--- final-godot-project/player_ui.gd
+++ final-godot-project/player_ui.gd
@@ -11,63 +11,44 @@ func actor_setup():
 	await get_tree().physics_frame
 	$".."/".."/Player.can_move = false
 	$InteractContainer.visible = false
-	$InteractBG.visible = false
-	$UpdateBG.visible = true
-	$UpdateContainer.visible = true
-	$UpdateContainer/Label.text = "There is an alien"
-	await get_tree().create_timer(3).timeout
-	$UpdateContainer/Label.text = "Reach the other side"
-	await get_tree().create_timer(3).timeout
-	$UpdateContainer/Label.text = "For this, find 2 keys"
-	await get_tree().create_timer(3).timeout
-	$UpdateBG.visible = false
-	$UpdateContainer.visible = false
-	$".."/".."/Player.can_move = true
+	$UpdateUI.visible = true
+	$InventoryContainer/Item1.visible = false
+	$InventoryContainer/Item2.visible = false
+	$InventoryContainer/Item3.visible = false
 
 func show_interact():
 	$InteractContainer.visible = true
-	$InteractBG.visible = true
 	
 func hide_interact():
 	$InteractContainer.visible = false
-	$InteractBG.visible = false
 	
 # increment num keys and show a message 
 func set_num_keys(numkeys):
-	$InventoryBG.visible = true
-	$InventoryContainer.visible = true
-	$InventoryContainer/Label.text = "Inventory: " + str(numkeys) + " keys"
-	$UpdateBG.visible = true
-	$UpdateContainer.visible = true
-	$UpdateContainer/Label.text = "You just found a key!"
-	await get_tree().create_timer(1).timeout
-	$UpdateBG.visible = false
-	$UpdateContainer.visible = false
+	get_node("InventoryContainer/Item" + str(numkeys)).visible = true
 	
 func show_blank_message():
-	$UpdateBG.visible = true
-	$UpdateContainer.visible = true
-	$UpdateContainer/Label.text = "Empty box - no keys"
+	$UpdateUI.visible = true
+	$UpdateUI/Label.text = "Empty box - no keys"
 	await get_tree().create_timer(1).timeout
-	$UpdateBG.visible = false
-	$UpdateContainer.visible = false
+	$UpdateUI.visible = false
 
 func lose_screen():
-	$UpdateBG.visible = true
-	$UpdateContainer.visible = true
-	$UpdateContainer/Label.text = "You died. Game over"
+	$UpdateUI.visible = true
+	$UpdateUI/Label.text = "You died. Game over"
 	
 func game_win():
-	$UpdateBG.visible = true
-	$UpdateContainer.visible = true
-	$UpdateContainer/Label.text = "You won! Game over"
+	$UpdateUI.visible = true
+	$UpdateUI/Label.text = "You won! Game over"
 	
 func reqs_message():
 	hide_interact()
-	$UpdateBG.visible = true
-	$UpdateContainer.visible = true
-	$UpdateContainer/Label.text = "Insufficent resources"
+	$UpdateUI.visible = true
+	$UpdateUI/Label.text = "Insufficent resources"
 	await get_tree().create_timer(1).timeout
 	show_interact()
-	$UpdateBG.visible = false
-	$UpdateContainer.visible = false
+	$UpdateUI.visible = false
+
+
+func _on_button_pressed():
+	$UpdateUI.visible = false
+	$".."/".."/Player.can_move = true
